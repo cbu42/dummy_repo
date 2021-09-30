8dd92bb219609ccc10d9c1499b1fd54a2ed26c96
Casey B <cbu42@stanford.edu>
2021-09-30 14:50:09 -0700

    replicated eyetracking figure 2c from ryskin at end of graphs script

--- analysis/ryskin_et_al/1_incremental/main/rscripts/graphs.R
+++ analysis/ryskin_et_al/1_incremental/main/rscripts/graphs.R
@@ -130,15 +130,12 @@ length(toexclude$workerid)/length(accuracy$workerid) #0.06349206 data loss
 df = df %>% 
   filter(!workerid %in% toexclude$workerid)
 
-#unique(demo$language) # no exlusions (some people left it blank?)
-
 # # remove trials with incorrect selections
 df = df %>%
   filter(selection_correct==1)
 
 nrow(df) # 936 for pilot, 14745 for main
 
-# MAKE SURE THE ABOVE EXCLUSION CODE DOES THE RIGHT THING
 # get only experimental trials (no fillers) for further analysis
 # as a reminder we can use all trial types for testing our linking assumption
 d_test = df %>% 
@@ -205,7 +202,7 @@ toplot =  df %>%
   mutate(cond=fct_relevel(cond,"contrast", "no_contrast")) %>%
   mutate(window=fct_recode(window,prior="click_prior",adjective="click_adj",noun="click_noun")) %>% 
   mutate(window = fct_relevel(window,"prior","adjective"))
-dodge=position_dodge(0)
+  dodge=position_dodge(0)
 
 fig13 <- ggplot(toplot, aes(x=window, y=target, color=cond, linetype=pragContext,group=interaction(cond,pragContext))) +
   geom_line(size=1.3,position=dodge) +
@@ -268,7 +265,6 @@ ggsave(ryskin_f2, file="../graphs/ryskin_fig2_rep.pdf",width=9,height=4.5)
 # Does the bad pragContext make us increasingly less likely to draw a contrastive inference?
 # Subset to Adjective window bc that's where contrast effect shows up. 
 # Plot contrast v no contrast and bad s good just in the adj window. X axis is trial number (over time analysis)
-#defunct
 toplot =  d_test %>%
   select(workerid,response_times, pragContext,cond,click_adj,loc_target_pic,loc_competitor_pic,loc_contrast,loc_big_filler,loc_small_filler,instruction,trial_number) %>%  
   mutate(selection = click_adj) %>% 
@@ -291,19 +287,6 @@ toplot =  d_test %>%
   mutate(pragContext = fct_relevel(pragContext,"reliable","unreliable")) #%>% 
 #mutate(cond = fct_relevel(cond,"no_contrast","contrast"))
 view(toplot)
-over_time = ggplot(toplot, aes(x=trial_number, y=response_adj, color=pragContext, linetype=cond, group=interaction(pragContext, cond))) +
-  #geom_line(aes(color=pragContext, linetype= cond),size=1.3) +
-  geom_smooth(method='lm')+
-  labs(color="Pragmatic context", linetype = "Condition") +
-  #scale_alpha_discrete(range=c(.5,1))+
-  #facet_grid(cond ~ pragContext ) + 
-  scale_color_manual(values=c(ryskinPalette[1], ryskinPalette[3])) +
-  xlab("Trial number (sequentially)") +
-  ylab("Reaction time (ms)") +
-  theme(axis.text.x=element_text(angle=30,hjust=1,vjust=1))
-over_time
-ggsave(over_time, file="../graphs/response-over_time.pdf",width=9,height=4.5)
-
 toplot_target <- toplot %>% 
   filter(Region=='target')
 contrastive_inf = ggplot(toplot_target, aes(x=trial_number, y=Mean, color=pragContext, linetype=cond, shape = cond, group=interaction(pragContext, cond))) +
@@ -407,12 +390,6 @@ prior_df = read_tsv(paste0(getwd(),"/ryskin_eyetracking/PragTrain3_baseline_wind
     compet_prop = compet_dur/total_dur,
     other_prop = other_dur/total_dur,
     timebin_rel = timebin/10 - 17)
-#lots of zeros (and NaN and NAs)
-view(prior_df[1:100,])
-table(prior_df$total_dur)
-table(prior_df$target_dur)
-table(prior_df$compet_fix)
-nrow(prior_df)
 
 pragtrain3_list1 = read_tsv(paste0(getwd(),'/ryskin_eyetracking/experiments_2-3_trials_list_1.txt'))
 pragtrain3_list2 = read_tsv(paste0(getwd(),'/ryskin_eyetracking/experiments_2-3_trials_list_2.txt'))
@@ -553,7 +530,12 @@ toplot = left_join(toplot_select, toplot_eye, by=c("cond","pragContext","window"
   mutate(window = fct_relevel(window,"prior","adjective")) %>% 
   mutate(ymin_selections = as.numeric(ymin_selections),ymax_selections = as.numeric(ymax_selections),prop_selections = as.numeric(prop_selections))
 nrow(toplot)
-toplot
+names(toplot)
+toplot2 = left_join(toplot_select, toplot_eye, by=c("cond","pragContext","window","Region", "timebin")) %>% 
+  mutate(window = fct_relevel(window,"prior","adjective")) %>% 
+  mutate(ymin_selections = as.numeric(ymin_selections),ymax_selections = as.numeric(ymax_selections),prop_selections = as.numeric(prop_selections))
+nrow(toplot)
+names(toplot)
 
 ggplot(toplot, aes(x=prop_selections,y=prop_looks,color=Region,shape=cond)) +
   geom_point() +
@@ -590,6 +572,8 @@ ggplot(toplot, aes(x=prop_selections,y=prop_looks,color=Region,shape=pragContext
   geom_abline(intercept=0,slope=1,color="gray",linetype="dashed") +
   geom_smooth(method="lm") +
   geom_point() +
+  #new facet
+  facet_wrap(~ window) +
   #geom_errorbar(aes(ymin=ymin_looks,ymax=ymax_looks),width=0) +
   #geom_errorbarh(aes(xmin=ymin_selections,xmax=ymax_selections),height=0) +
   scale_color_manual(values=c(cbPalette[7],cbPalette[1],cbPalette[4],cbPalette[5])) +
@@ -600,9 +584,10 @@ ggplot(toplot, aes(x=prop_selections,y=prop_looks,color=Region,shape=pragContext
     y="Proportion of looks (Ryskin et al 2019, Exp. 2)") +
   xlim(0,1) +
   ylim(0,1) 
+ggsave("../graphs/corr_overall_faceted_by_window.pdf",width=6.5,height=3.5)
 ggsave("../graphs/corr_overall.pdf",width=5.5,height=3.5)
 
-#TODOL report these correlations (w/ facets, report the correlations with it)
+#TODO report these correlations (w/ facets, report the correlations with it)
 #facet by window
 #also include by-item for unreliable
 #selections on x axis, corresponding eye movements on y axis
@@ -680,4 +665,50 @@ d_noun = toplot %>%
 
 m.noun = lm(prop_looks ~ cprop_selections*Region, data=d_noun)
 summary(m.noun)
+
+# TODO: make me a plot!
+names(pragtrain3_data)
+# ggplot(toplot, aes(x=prop_selections,y=prop_looks,color=Region,shape=pragContext)) +
+names(toplot_eye)
+
+toplot_time = pragtrain3_crit %>% 
+  separate(audio,into=c("noun","size","wav")) %>% 
+  # cat_small.
+  # select(contrast_cond,prag_context_cond,timebin,noun,target_fix,compet_fix) %>% 
+  select(contrast_cond,prag_context_cond,timebin,target_fix,compet_fix) %>% 
+  filter(!(target_fix == 1 & compet_fix == 1)) %>% 
+  mutate(other_fix = ifelse(target_fix + compet_fix == 0, 1, 0),
+         window = ifelse(timebin <=200, "prior", ifelse(timebin <= 830, "adjective","noun"))) %>% 
+  pivot_longer(names_to = "Region", values_to = "fixation",cols=target_fix:other_fix) %>%
+  mutate(Region = fct_recode(Region,"target" = "target_fix","competitor"="compet_fix","other"="other_fix")) %>% 
+  # group_by(contrast_cond,prag_context_cond,window,Region,noun) %>% 
+  group_by(contrast_cond,prag_context_cond,window,Region, timebin) %>% 
+  #summarize(prop_looks=mean(fixation),ci_low_looks=ci.low(fixation),ci_high_looks=ci.high(fixation)) %>%
+  summarize(prop_looks=mean(fixation)) %>% 
+  ungroup() %>% 
+  #mutate(ymin_looks=prop_looks-ci_low_looks,ymax_looks=prop_looks+ci_high_looks) %>% 
+  rename(pragContext = prag_context_cond, cond = contrast_cond)
+toplot_time <- filter(toplot_time, Region != "other")
+toplot_time$condition <- paste0(toplot_time$cond, ", ",toplot_time$pragContext)
+toplot_time <- mutate(toplot_time, Region = fct_relevel(Region,"target","competitor"))
+
+unique(toplot_time$condition)
+view(ryskinPalette)
+
+ggplot(toplot_time, aes(x=timebin, y=prop_looks)) +
+  geom_line(size=1, aes(color=condition,linetype=Region)) +
+  #geom_ribbon(aes(ymin=ymin,ymax=ymax,fill=determiner,group=interaction(determiner,size)),alpha=.3) +
+  scale_fill_manual(values=c(ryskinPalette[1],ryskinPalette[3],ryskinPalette[2], ryskinPalette[4])) +
+  scale_color_manual(values=c(ryskinPalette[1],ryskinPalette[3],ryskinPalette[2], ryskinPalette[4])) +
+  geom_vline(aes(xintercept=200)) +
+  geom_vline(aes(xintercept=830)) +
+  #geom_text(data=windows,aes(label=window,x=x),y=.9,size=2) +
+  xlab("Time in ms relative to audio onset") +
+  ylab("Proportion of looks") +  
+  scale_x_continuous(breaks=seq(-200,1800,by=200),minor_breaks = seq(-200,1800,by=200)) +
+  #facet_wrap(~cond)
+# theme(axis.text.x=element_text(angle=30,hjust=1,vjust=1))
+ggsave("../graphs/ryskin_fig2c_recreated.pdf",width=5.5,height=3.5)
+ggsave("../graphs/ryskin_fig2c_recreated.png",width=5.5,height=3.5)
+
 
